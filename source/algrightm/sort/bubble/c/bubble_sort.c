38551a1acc5cf744a9c9c442b083e583bf66880b
XieLong <1293336945@qq.com>
2019-03-15 11:31:22 +0800

    修改：     bubble_sort.c

--- source/algrightm/sort/bubble/c/bubble_sort.c
+++ source/algrightm/sort/bubble/c/bubble_sort.c
@@ -3,6 +3,10 @@
  *
  * @author skywang
  * @date 2014/03/11
+ *
+ * modified:
+ * by XieLong
+ * date 2019/03/15
  */
 
 #include <stdio.h>
@@ -15,72 +19,92 @@
 /*
  * 冒泡排序
  *
+ * 返回值说明：
+ *     s -- 算法循环的次数
  * 参数说明：
  *     a -- 待排序的数组
  *     n -- 数组的长度
  */
-void bubble_sort1(int a[], int n)
+int bubble_sort1(int a[], int n)
 {
-	int i,j;
+	int i, j, s = 0;
 
-	for (i=n-1; i>0; i--)
+	for (i = n-1; i > 0; i--)
 	{
         // 将a[0...i]中最大的数据放在末尾
-		for (j=0; j<i; j++)
+		for (j = 0; j < i; j++)
 		{
 			if (a[j] > a[j+1])
+            {
 				swap(a[j], a[j+1]);
+            }
+            s++;
 		}
 	}
+    return s;
 }
 
 /*
  * 冒泡排序(改进版)
  *
+ * 返回值说明：
+ *     s -- 算法循环的次数
  * 参数说明：
  *     a -- 待排序的数组
  *     n -- 数组的长度
  */
-void bubble_sort2(int a[], int n)
+int bubble_sort2(int a[], int n)
 {
-	int i,j;
+	int i, j, s = 0;
 	int flag; 				// 标记
 
-	for (i=n-1; i>0; i--)
+	for (i = n-1; i > 0; i--)
 	{
 		flag = 0;			// 初始化标记为0
 
         // 将a[0...i]中最大的数据放在末尾
-		for (j=0; j<i; j++)
+		for (j = 0; j < i; j++)
 		{
 			if (a[j] > a[j+1])
 			{
 				swap(a[j], a[j+1]);
 				flag = 1;	// 若发生交换，则设标记为1
 			}
+            s++;            // 记录循环次数
 		}
 
-		if (flag==0)
+		if (flag == 0)
 			break;			// 若没发生交换，则说明数列已有序。
 	}
+    return s;
 }
 
-void main()
+int main()
 {
 	int i;
-	int a[] = {20,40,30,10,60,50};
-	int ilen = LENGTH(a);
+	int a[] = {10,20,40,30,10,60,50,70};
+    int b[] = {10,20,40,30,10,60,50,70};
+	int alen = LENGTH(a);
+    int blen = LENGTH(b);
 
 	printf("before sort:");
-	for (i=0; i<ilen; i++)
+	for (i = 0; i < alen; i++)
 		printf("%d ", a[i]);
 	printf("\n");
 
-	bubble_sort1(a, ilen);
-	//bubble_sort2(a, ilen);
+	int as = bubble_sort1(a, alen);
+	int bs = bubble_sort2(b, blen);
 
-	printf("after  sort:");
-	for (i=0; i<ilen; i++)
+	printf("after sort1:");
+	for (i = 0; i < alen; i++)
 		printf("%d ", a[i]);
-	printf("\n");
+	printf("\nafter sort2:");
+    for (i = 0; i < blen; i++)
+        printf("%d ", b[i]);
+    printf("\n");
+
+    printf("bubble_sort1 used %d time(s) to swap.\n", as);
+    printf("bubble_sort2 used %d time(s) to swap.\n", bs);
+
+    return 0;
 }
